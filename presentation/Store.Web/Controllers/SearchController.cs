cfa0d092e565a61ff38942e565d5d16edb52451b
Азамат Рахимбаев <azamat.rakhimbayev@corp.kfu.kz>
2024-07-25 16:23:29 +0500

    Добавить первый урок в гит хаб, так в начале этот момент не получился.

---
+++ presentation/Store.Web/Controllers/SearchController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace Store.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IBookRepository bookRepository;
+        public SearchController(IBookRepository bookReporsitory)
+        {
+            this.bookRepository = bookReporsitory;
+        }
+        public IActionResult Index(string query)
+        {
+            var books = bookRepository.GetAllByTitle(query);
+            return View(books);
+        }
+    }
+}
