1861d29ef3996188478d0bfabc5611d172eee843
Азамат Рахимбаев <azamat.rakhimbayev@corp.kfu.kz>
2024-07-31 10:04:29 +0500

    Реализовать поиск по ИСБТ и автору

--- domain/Store/Book.cs
+++ domain/Store/Book.cs
@@ -1,14 +1,26 @@
-﻿namespace Store;
+﻿using System.Text.RegularExpressions;
+
+namespace Store;
 
 public class Book
 {
     public int Id { get; }
+    public string Isbn { get; }
+    public string Author { get; }
     public string Title { get; }
 
-    public Book(int id, string title)
+    public Book(int id, string Isbn, string Autor, string title)
     {
 
         Id = id;
         Title = title;
+        Author = Autor;
+        Isbn = Isbn;
+    }
+    internal static bool IsIsbn(string s)
+    {
+        if (s == null) return false;
+        s = s.Replace("-", "").Replace(" ", "").ToUpper();
+        return Regex.IsMatch(s, @"^ISBN\d{10}(\d{3})?$");
     }
 }
